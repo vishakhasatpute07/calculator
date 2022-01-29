787695a7f94e5b1c1457bc334ac5dd702eaac728
vishakha <Vishw@DESKTOP-SMSDJR0>
2022-01-29 11:56:47 +0530

    First commit

---
+++ methodExecution/src/methodExecution/CallMethodDemo.java
@@ -0,0 +1,22 @@
+package methodExecution;
+
+public class CallMethodDemo {
+	int val=150;
+
+	int operation(int val) {
+		val =val*10/100;
+		return(val);
+	}
+
+	public static void main(String[] args) {
+		
+		CallMethodDemo d = new CallMethodDemo();
+		System.out.println("Before operation value of data is "+d.val);
+		d.operation(100);
+		System.out.println("After operation value of data is "+d.val);
+		}
+
+
+	}
+
+
